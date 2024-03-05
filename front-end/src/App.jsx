1c7244d98ec18830abb71bff34ac69009aad3a84
AhmedA231 <127413249+AhmedA231@users.noreply.github.com>
2024-03-05 11:51:30 +0000

    Plain Pages added with correct React Router Route Setup.
    Nav Bar Tabs Styled  Correctly.
    
    Tidied Code

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,10 +1,32 @@
-import { useState } from 'react'
-import Header from './components/Sidebar/Header.jsx'
-function App() {
-  
-  return (
-    <Header />
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+
+// Pages
+import Home from './pages/Home.jsx';
+import Saved from './pages/Saved.jsx';
+import Search from './pages/Search.jsx'
+
+// Layouts
+import RootLayout from './layouts/RootLayout.jsx';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path='saved' element={<Saved />} />
+      <Route path='search' element={<Search />} />
+    </Route>
   )
+);
+
+function App() {
+  return (
+    <RouterProvider router={router} /> 
+  );
 }
 
-export default App
+export default App;
