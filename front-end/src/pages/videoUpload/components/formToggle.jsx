3ea2695f805503ef291179addfd1996555bbd966
AhmedA231 <webdev.ahmed23@gmail.com>
2024-03-26 16:21:02 +0000

    still a little broken

---
+++ front-end/src/pages/videoUpload/components/formToggle.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+function FormToggle({ showForm, onShowExistingCourseForm, onShowNewCourseForm }) {
+  return (
+    <div className="d-flex gap-2 mb-5 justify-content-center">
+      <Button
+        style={{
+          transition: "all 0.3s ease-in-out",
+          width: showForm === "existing" ? "50%" : "25%",
+          height: "50px",
+          backgroundColor: showForm === "existing" ? "#BDA1CC" : "transparent",
+          borderColor: "#BDA1CC",
+          color: "#000",
+        }}
+        onClick={onShowExistingCourseForm}
+      >
+        Add Video
+      </Button>
+      <Button
+        style={{
+          transition: "all 0.3s ease-in-out",
+          width: showForm === "new" ? "50%" : "25%",
+          height: "50px",
+          backgroundColor: showForm === "new" ? "#BDA1CC" : "transparent",
+          borderColor: "#BDA1CC",
+          color: "#000",
+        }}
+        onClick={onShowNewCourseForm}
+      >
+        Create Course
+      </Button>
+    </div>
+  );
+}
+
+export default FormToggle;
